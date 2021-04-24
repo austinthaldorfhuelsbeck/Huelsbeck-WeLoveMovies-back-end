db199da3905bd6ed5da8d0659625f0a6d2b7676c
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-04-23 17:05:40 -0700

    catching up

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES
+if (process.env.USER) require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
