3dacf3ef4808a1de0611e355f3a637eb21d52cca
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-04-23 19:26:20 -0700

    movieExists

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,18 +1,26 @@
 const service = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+async function movieExists(req, res, next) {
+  const movie = await service.read(req.params.movieId);
+  if (movie[0]) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: "Movie cannot be found." });
+}
+
 async function list(req, res) {
   const data = await service.list();
   res.json({ data });
 }
 
-async function read(req, res) {
-  const id = req.params.movieId;
-  const data = await service.read(id);
-  res.json({ data });
+function read(req, res) {
+  const { movie: data } = res.locals;
+  res.json({ data: data[0] });
 }
 
 module.exports = {
   list: [asyncErrorBoundary(list)],
-  read: [asyncErrorBoundary(read)],
+  read: [asyncErrorBoundary(movieExists), read],
 };
