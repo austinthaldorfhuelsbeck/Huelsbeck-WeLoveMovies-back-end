f6bd39e105fda1d9bffb7d6c069c3637d9a5a2be
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-04-27 15:38:41 -0700

    connecting to test

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,23 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+const addMovies = mapProperties({
+  movie_id: "movies.movie_id",
+  title: "movies.title",
+  runtime_in_minutes: "movies.runtime_in_minutes",
+  rating: "movies.rating",
+  description: "movies.description",
+  image_url: "movies.image_url",
+  is_showing: "movies.is_showing",
+});
 
 function list() {
-  return knex("theaters").select("*");
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "mt.movie_id", "m.movie_id")
+    .select("t.*", "m.*", "mt.is_showing")
+    .first()
+    .then(addMovies);
 }
 
 module.exports = {
