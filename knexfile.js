4bfe392f30ef6aaa8545726730036f758016ec8c
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-01-11 13:04:11 -0800

    knex config

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,48 @@
+// Knex configuration
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
 
+// Enviornment Variable handling
+const {
+  DATABASE_HOST,
+  DATABASE_USER_DEVELOPMENT,
+  DATABASE_PASSWORD_DEVELOPMENT,
+  DATABASE_USER_PRODUCTION,
+  DATABASE_PASSWORD_PRODUCTION
+} = process.env;
+
+// Config and export
 module.exports = {
   development: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
-
-  test: {
-    client: "sqlite3",
+    client: 'pg',
     connection: {
-      filename: ":memory:",
+      host: DATABASE_HOST,
+      user: DATABASE_USER_DEVELOPMENT,
+      database: DATABASE_USER_DEVELOPMENT,
+      password: DATABASE_PASSWORD_DEVELOPMENT,
+      charset: 'utf8'
     },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, "src", "db", "migrations")
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
+      directory: path.join(__dirname, "src", "db", "seeds")
+    }
   },
+  production: {
+    client: 'pg',
+    connection: {
+      host: DATABASE_HOST,
+      user: DATABASE_USER_PRODUCTION,
+      database: DATABASE_USER_PRODUCTION,
+      password: DATABASE_PASSWORD_PRODUCTION,
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations")
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds")
+    }
+  }
 };
